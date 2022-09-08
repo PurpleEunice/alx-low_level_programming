bdaf8913b813377e6d69027e9caafd50f47ccb4c
PurpleEunice <euniceoke2022@gmail.com>
2022-09-08 12:44:09 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+/**
+ *
+ * * main - print the string in the put function
+ *
+ * *
+ *
+ * * Descripton: using the main function
+ *
+ * * this program prints "Programming is like building a multilingual puzzle
+ *
+ * *Return: 0
+ *
+ * */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
