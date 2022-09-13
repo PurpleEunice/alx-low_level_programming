a18f7882ab4f23cd29ce84666d0eeb8552e042b7
PurpleEunice <euniceoke2022@gmail.com>
2022-09-13 13:03:09 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,28 +1,37 @@
-#include <stdio.h> 
+#include "stdio.h"
 
 /**
- * print_to_98 - Prints all natural numbers 
- * from n to 98.
- *
- * @n: input number.
- * Return: no return.
+ * print_to_98 - print all the numbers between the provided number and 98
+ * @n: integer to start at
+ * Return: Void.
  */
+
 void print_to_98(int n)
 {
-	if (n > 98) 
-	{
-		for (; n > 98; n--) 
-		{
-			printf("%d, ", n); 
-		}
-	}
-	else if (n < 98)
-	{
-		for (; n < 98; n++)
-		{
-			printf("%d, ", n);
-		}
-	}
-	printf("%d\n", n);
-}
+	int i = 0;
 
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i != n)
+			{
+				printf(", ");
+			}
+			printf("%d", i);
+		}
+
+	}
+	else
+	{
+		for (i = n; i <= 98 ; i++)
+		{
+			if (i != n)
+			{
+				printf(", ");
+			}
+			printf("%d", i);
+		}
+	}
+	putchar('\n');
+}
